003443d582d6b706de85f1ba906741dcd7c512a4
Your Name <you@example.com>
2018-09-15 09:56:06 -0400

    committed

---
+++ w/es5/tile.js
@@ -0,0 +1,281 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/**
+ * Tile Component<BR>
+ * <BR><BR><img src=../img/tile.png width=30% style="border:1px lime dashed;padding:20px">
+ * <BR><BR><a href="../html/tile.html">DEMO</a>
+ */
+var Tile = function (_HTMLElement) {
+	_inherits(Tile, _HTMLElement);
+
+	function Tile() {
+		_classCallCheck(this, Tile);
+
+		console.group("Tile.constructor");
+
+		var _this = _possibleConstructorReturn(this, (Tile.__proto__ || Object.getPrototypeOf(Tile)).call(this));
+
+		console.groupEnd();
+		return _this;
+	}
+
+	_createClass(Tile, [{
+		key: "connectedCallback",
+
+
+		/**
+   * This function is called when this is attached to DOM
+   * @connectedCallback. 
+   */
+		value: function connectedCallback() {
+			var _this2 = this;
+
+			console.group("Tile.connectedCallback");
+
+			var self = this;
+
+			// GET PROPERTIES AND INTERESTING ELEMENTS
+			this._initialize();
+
+			// REPLACE CONTENT IF NECESSARY WITH NEW STUFF
+			this.innerHTML = "\n\t    <div class=\"row h-100 justify-content-center align-items-center\">\n\t\t<div>\n\t\t" + this.dom.content + "\n\t\t</div>\n\t    </div>";
+
+			if (this.properties.clickable == "true") {
+				$(this).addClass("clikable");
+
+				this.addEventListener("click", function (e) {
+					wc.publish(_this2, "wc-tile", {
+						time: new Date().getTime(),
+						action: "click",
+						id: self.id,
+						uparam: self.properties.uparam
+					});
+				});
+			}
+
+			// ADD STATS AND OTHER FINAL STUFF
+			this._finalize();
+
+			console.groupEnd();
+		}
+	}, {
+		key: "attributeChangedCallback",
+
+
+		/**
+   * Called with .setAttribute(...) function call
+   * @attributeChangedCallback
+   */
+		value: function attributeChangedCallback(attr, oldval, newval) {
+			console.group("Tile.attributeChangedCallback:", attr, oldval, newval);
+
+			this.properties = this.properties || [];
+
+			var obs = Tile.observedAttributes;
+
+			for (var i = 0; i < obs.length; i++) {
+				if (newval) {
+					this.properties[obs[i]] = newval;
+				}
+			}
+
+			// YOUR CODE FOR CHANGES GO HERE (MAYBE NULL FIRST TIME THROUGH)
+			try {
+				switch (attr) {
+					case "header":
+						break;
+
+					default:
+						break;
+				}
+			} catch (e) {
+				console.warn(e.name + ' > ' + e.message);
+			}
+
+			console.groupEnd();
+		}
+	}, {
+		key: "_fetchElements",
+
+
+		/**
+   * Stores DOM elements of interest for future use
+   * @_fetchElements
+   */
+		value: function _fetchElements() {
+			console.group("Tile._fetchElements");
+
+			this.dom = this.dom || [];
+			this.dom.content = this.innerHTML;
+
+			console.groupEnd();
+		}
+	}, {
+		key: "_fetchAttributes",
+
+
+		/**
+   * Component attributes are _fetched and defaults are set if undefined
+   * @_fetchAttributes
+   * @param {string} [modal=true] mode for our tile
+   */
+		value: function _fetchAttributes() {
+			console.group("Tile._fetchAttributes");
+
+			this.properties = {
+				cname: "Tile",
+				author: "Mel Heravi",
+				version: "1.0",
+				clickable: "false"
+			};
+
+			// SAVE WIDGET SPECIFIC PROPERTIES
+			this.propertiesW = [];
+
+			// SAVE ALL OTHER PROPERTIES
+			var attrs = wc.getAttributes(this);
+
+			for (var key in attrs) {
+				var attr = this.getAttribute(key) || this.properties.key;
+				this.properties[key] = this.getAttribute(key);
+				this.propertiesW[key] = this.getAttribute(key);
+				console.log(key + ": " + attrs[key]);
+			}
+
+			// SET ALL INITIAL ATTRIBUTES
+			for (var key in this.properties) {
+				switch (key) {
+					case "header":
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			console.log("ATTRIBUTES: ", this.properties);
+
+			console.groupEnd();
+		}
+	}, {
+		key: "destroy",
+
+
+		/**
+   * Destroy the instance object and artifacts
+   * @destroy
+   */
+		value: function destroy() {
+			console.group("Tile.destroy:", this.id);
+
+			// FREE ALL MEMORY
+			// you should delete all created objects here
+
+			// FREE POINTER
+			delete this;
+
+			// REMOVE ITEM FROM DOM
+			this.parentNode.removeChild(this);
+
+			console.groupEnd();
+		}
+	}, {
+		key: "configure",
+
+
+		/**
+   * configure the instance object and artifacts
+   * @configure
+   */
+		value: function configure(options) {
+			console.group("Tile.configure:", JSON.stringify(options));
+
+			// PROCESS ALL OPTIONS HERE
+
+			console.groupEnd();
+		}
+	}, {
+		key: "_initialize",
+
+
+		/**
+   * SAVE DATA FOR ANALYTICS
+   * @_initialize
+   */
+		value: function _initialize() {
+			console.group("Tile._initialize:", this.id);
+
+			// FETCH ALL INTERESTING ELEMENTS
+			this._fetchElements();
+
+			// FETCH ALL ATTRIBUTES
+			this._fetchAttributes();
+
+			console.groupEnd();
+		}
+	}, {
+		key: "_finalize",
+
+
+		/**
+   * SAVE DATA FOR ANALYTICS
+   * @_finalize
+   */
+		value: function _finalize() {
+			console.group("Tile._finalize:", this.id);
+
+			this.classList.add("wc");
+
+			// ADD ANALYTICS HERE
+			wc.setStats(this, this.properties.cname, this.properties.version);
+
+			// SHOW IT NOW (NO FLICKERS) 
+			this.style.visibility = "visible";
+
+			console.groupEnd();
+		}
+	}, {
+		key: "disconnectedCallback",
+
+
+		/**
+   * Invoked When component is removed. Usually with a .remove() function call
+   * @disconnectedCallback
+   */
+		value: function disconnectedCallback() {
+			console.group("Tile.disconnectedCallback");
+
+			/* CLEAN UP NOW */
+
+			console.groupEnd();
+		}
+	}], [{
+		key: "observedAttributes",
+
+
+		/**
+   * Set observable values here. When Changed, attributeChangedCallback is invoked
+   * @observedAttributes
+   */
+		get: function get() {
+			console.group("Tile.observedAttributes");
+
+			this.observables = [];
+
+			console.groupEnd();
+			return this.observables;
+		}
+	}]);
+
+	return Tile;
+}(HTMLElement);
+
+window.customElements.define('wc-tile', Tile);
