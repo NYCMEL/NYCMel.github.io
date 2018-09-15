d8837479f58b5a15d52d876fa43db3d718ce0b29
Mel <melheravi@gmail.com>
2018-09-15 19:39:33 -0400

    committed

--- w/req/dropdown.tcl
+++
@@ -1,8 +0,0 @@
-array set req {
-    dropdown {
-	import "bootstrap";
-	
-        import "./w/scss/_dropdown.scss";
-        import "./w/js/dropdown";
-    }
-}
--- w/req/slide.tcl
+++
@@ -1,9 +0,0 @@
-array set req {
-    slide {
-        import "./w/scss/ext/_sidr.dark.scss";
-	import "sidr/dist/jquery.sidr.js";
-	
-        import "./w/scss/_slide.scss";
-        import "./w/js/slide";
-    }
-}
