28a43fe1327d4a2d7b19da7579bec4d99166a1e1
Mel <melheravi@gmail.com>
2018-09-16 17:35:03 -0400

    committed

--- w/js/ext/btns.js
+++ w/js/ext/btns.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////////
-//// Time-stamp: <2018-08-12 12:44:05 (melify)>
+//// Time-stamp: <2018-09-16 17:33:39 (melify)>
 /////////////////////////////////////////////////////////////////////////////////
 var mtkBtns = {};
 
@@ -16,19 +16,19 @@ mtkBtns.init = function(options) {
 	switch(id) 
 	{
 	    case "mtk-btn-1":
-	    document.location.href = "/Melify/mtk/dev/tk/lib/components/w/doc"
+	    document.location.href = "/tk/lib/components/w/doc"
 	    break;
 
 	    case "mtk-btn-2":
-	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/Melify/mtk/dev/tk/lib/components/w/html/" + name + ".html";
+	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/tk/lib/components/w/html/" + name + ".html";
 	    break;
 
 	    case "mtk-btn-3":
-	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/Melify/mtk/dev/tk/lib/components/w/js/" + name + ".js";
+	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/tk/lib/components/w/js/" + name + ".js";
 	    break;
 
 	    case "mtk-btn-4":
-	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/Melify/mtk/dev/tk/lib/components/w/scss/_" + name + ".scss";
+	    document.location.href = "/mtk/render?callback=tk::viewsource&file=/tk/lib/components/w/scss/_" + name + ".scss";
 	    break;
 	}
     });
